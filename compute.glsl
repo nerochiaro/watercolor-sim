90f60f97b7948b7ae209f9a7004473949a5ddcd7
Ugo Riboni <ugo.riboni@zoundream.com>
2025-10-19 10:55:40 +0200

    Display simulation initial state

--- compute.glsl
+++ compute.glsl
@@ -1,27 +1,50 @@
 #[compute]
 #version 450
 
+/****************** DO NOT CHANGE FROM HERE *********************/
+
 // Invocations in the (x, y, z) dimension.
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
-// Our texture
+// Shared texture for output. Read by fragment shader for rendering.
 layout(r32f, set = 0, binding = 0, rgba32f) uniform restrict writeonly image2D out_image;
 
-// Our push PushConstant.
+/****************** DO NOT CHANGE TO HERE *********************/
+
+// Simulation parameters as push constant
 layout(push_constant, std430) uniform Params {
-	vec2 texture_size;
+	vec2 size;
 } params;
 
-// // A binding to the buffer we create in our script
-// layout(set = 0, binding = 0, std430) restrict buffer WetnessBuffer {
-//     int data[];
-// } wet;
+// Simulation buffer for wetness level
+layout(set = 1, binding = 0, std430) restrict buffer WetnessBuffer {
+    int data[];
+} wet;
+
+// Unpack the size and convert to int (as push constant is an array of floats)
+int width = int(params.size.x);
+int height = int(params.size.y);
+
+const int diffusion_limit = 268435456;
+const int _diagonal_reduction = 1518500249;
+const int int_max = 2147483647;
+
+int getv(int x, int y) {
+    return wet.data[width * y + x];
+}
+
+void setv(int x, int y, int value) {
+    wet.data[width * y + x] = value;
+}
 
 void main() {
-    int y = int(gl_GlobalInvocationID.y);
     int x = int(gl_GlobalInvocationID.x);
+    int y = int(gl_GlobalInvocationID.y);
 
-	vec4 color = vec4(0.0, 1.0, 0.0, 1.0);
+    int cell = getv(x, y);
+    float c = cell / float(int_max);
+
+	vec4 color = vec4(c, c, c, 1.0);
 	ivec2 coord = ivec2(x, y);
     imageStore(out_image, coord, color);
 }
