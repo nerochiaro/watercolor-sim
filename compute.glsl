cf52d4a7bf65c3b82e52b72c338560eec4b6d7e5
Ugo Riboni <ugo.riboni@zoundream.com>
2025-10-24 23:29:41 +0200

    Port the diffusion sim to the compute shader

--- compute.glsl
+++ compute.glsl
@@ -1,15 +1,11 @@
 #[compute]
 #version 450
 
-/****************** DO NOT CHANGE FROM HERE *********************/
-
 // Invocations in the (x, y, z) dimension.
-layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
 // Shared texture for output. Read by fragment shader for rendering.
 layout(r32f, set = 0, binding = 0, rgba32f) uniform restrict writeonly image2D out_image;
-
-/****************** DO NOT CHANGE TO HERE *********************/
 
 // Simulation parameters as push constant
 layout(push_constant, std430) uniform Params {
@@ -33,25 +29,70 @@ layout(set = 1, binding = 2, std430) restrict buffer SimDebug {
 int width = int(params.size.x);
 int height = int(params.size.y);
 
-const int diffusion_limit = 268435456;
-const int _diagonal_reduction = 1518500249;
 const int int_max = 2147483647;
+const int diffusion_limit = 4;
+const bool sample_diagonally = true;
+const int diagonal_reduction = 2;
 
 int getv(int x, int y) {
-    return wet.data[width * y + x];
+    return sim_read.data[width * y + x];
 }
 
 void setv(int x, int y, int value) {
-    wet.data[width * y + x] = value;
+    sim_write.data[width * y + x] = value;
+}
+
+int _wick(int from_x, int from_y, int to_x, int to_y) {
+	if (from_x < 0 || from_y < 0 || from_x >= width || from_y >= height) {
+		return 0;
+    }
+
+	int v = getv(to_x, to_y);
+	int o = getv(from_x, from_y);
+	if (v != 0 && o != 0) {
+		int diff = o - v;
+		if (diff != 0) {
+			return diff;
+        }
+    }
+	return 0;
+}
+
+int _process_cell(int x, int y) {
+	int v = getv(x, y);
+	int n = _wick(x, y - 1, x, y) / diffusion_limit;
+	int s = _wick(x, y + 1, x, y) / diffusion_limit;
+	int w = _wick(x - 1, y, x, y) / diffusion_limit;
+	int e = _wick(x + 1, y, x, y) / diffusion_limit;
+	int straight = n + s + w + e;
+
+	int diagonal = 0;
+	if (sample_diagonally) {
+		int ne = (_wick(x + 1, y - 1, x, y) / diffusion_limit) / diagonal_reduction;
+		int nw = (_wick(x - 1, y - 1, x, y) / diffusion_limit) / diagonal_reduction;
+		int se = (_wick(x + 1, y + 1, x, y) / diffusion_limit) / diagonal_reduction;
+		int sw = (_wick(x - 1, y + 1, x, y) / diffusion_limit) / diagonal_reduction;
+		diagonal = ne + nw  + se + sw;
+	}
+
+	int delta = straight + diagonal;
+	debug.data[x + width * y] = delta;
+	return v + delta;
 }
 
 void main() {
     int x = int(gl_GlobalInvocationID.x);
     int y = int(gl_GlobalInvocationID.y);
 
-    int cell = getv(x, y);
-    float c = cell / float(int_max);
+    if (x >= width || y >= height) {
+        return;
+    }
 
+    int cell = _process_cell(x, y);
+    setv(x, y, cell);
+
+    // Assign the value as greyscale to the output image texture
+    float c = cell / float(int_max);
 	vec4 color = vec4(c, c, c, 1.0);
 	ivec2 coord = ivec2(x, y);
     imageStore(out_image, coord, color);
